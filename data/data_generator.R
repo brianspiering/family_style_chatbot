05303be35894223d7b86fad78582e8b0ff61bc89
eugeneh101 <eugeneh101@yahoo.com>
2016-07-09 12:45:28 -0700

    fresh start

---
+++ data/data_generator.R
@@ -0,0 +1,95 @@
+# Singapore
+Sesame Oil Chicken Wings
+Singapore Satay
+Singapore Chili Crabs
+Singapore Noodle Curry Shrimp
+Singapore Sling Cocktail
+Singapore laksa
+
+
+# Mexico
+Great Chicken Taquitos
+Double Chili cheese Dip
+Grilled Fish Tacos
+Chili Rellenos
+Albondigas
+Vegetarian Tortilla Soup
+
+# Indonesia
+Indonesian Banana Fritters
+Indonesia Satay
+Indonesian Spiced Rice
+Indonesian Fried Noodles
+Indonesian Beef Rendang
+Oxtail soup
+
+# France
+Peach and Goat Cheese Tartine
+Creme brulee
+Crepe Suzette
+Ratatouille
+French Onion Soup
+French Beef Stew
+
+
+# China
+Chinese Tea Leaf Eggs
+Seseame Oil Chicken WIngs
+Chinese Mabo Tofu
+General Tsao's Chicekn'
+Chinese Sizzling Rice Soup
+Chinese Corn Soup
+
+
+# Vietnam
+Vietnamese Fresh Spring ROlls
+Vietnamese Salad Rolls
+Vietnamese Chicken Salad
+Vietnamese Aromatic Lamb Chops  
+Vietnamese Beef Pho
+Vietnamese Style Vegetarian Curry Soup
+
+# Japan
+Japanese-Style Grilled Mushrooms
+Tofu Hiyayakko
+Crispy Shrimp Tempura
+Miso Salmon
+Dashi Stock
+Beef Sukiyaki
+
+# India
+Crispy Vegetables Pakoras
+Green Chutney
+Indian Pizza
+Curried Chickpeas
+Indian Chai
+Mango-Mint Lassi
+
+
+# Thailand
+Fried Chicken Wings
+Fried Tofu with Peanut Dipping Sauce
+Barbeque Pork on Rice
+Beef Stew Recipe
+Thai Ginger Soup
+Thai Hout and Sour Soup
+
+# Italy
+Caprese Appetizier
+Prosciutto Cups
+Shrimp Scampi with Pasta
+Pork Chops Italiano
+Italian Halibut Chwoder
+Creamy Italian White Bean Soup
+
+
+persona_designer = function(score_range, portion_modifier, noise) {
+  # score_range will be [1, 10]
+  # portion_modifier will be [-1, 1]
+  score_range = sample(score_range, length(score_range))
+  portion_modifier = sample(portion_modifier, length(portion_modifier))
+  sapply(1:6
+         
+         
+}
+sample(1: 10, 10)
