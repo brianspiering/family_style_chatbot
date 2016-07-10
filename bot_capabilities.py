0389f235eec832a610cd0623fbdcb36f3347193e
Jon Hourany <jonhourany@gmail.com>
2016-07-10 14:18:55 -0700

    Added to bot conversations

--- bot_capabilities.py
+++ bot_capabilities.py
@@ -6,17 +6,33 @@ confirmation_messages = ["Okay.",
                         "Done.",
                         "Awesome!"]
 
+init_convo_messages = ["Did someone say lunch!?",
+                     "Who's hungry?!",
+                     "Sounds like someone's hungry! Who's in?"]
+
 BotDo = namedtuple("BotDo", ['response', 
                             'action'])
 
 # The key is the trigger word; The value is a tuple of text responses and actions
 bot_capabilities = {'test': BotDo("Command received", 
                             None),
-            'add_me': BotDo(confirmation_messages,
+            'me': BotDo(confirmation_messages,
                             "add_self_to_eaters"),
+            'lunch': BotDo(init_convo_messages,
+                            None),
+            'nevermind': BotDo(confirmation_messages,
+                            'remove_person_from_eaters'),
+            'different': BotDo(confirmation_messages,
+                            'generate_new_recommandations'),
+            'weather': BotDo(["It's always foggy in San Fransisco"],
+                            None),
+            'sandwich': BotDo(["Sorry :( I don't have arms!"],
+                            None)
             'add_person': BotDo(confirmation_messages, 
                             "add_person_to_eaters"),
-            'fit_model': BotDo(["Sounds good. Based on your group preferences here are my suggestions: ..."], 
+            'whats_your_favorite': BotDo(['Beer!', 'I could always go for pizza!'],
+                            None)
+            'go': BotDo(["Sounds good. Based on your group preferences here are my suggestions: ..."], 
                             "fit_model") # pass in list of users as arguments
             }
 
